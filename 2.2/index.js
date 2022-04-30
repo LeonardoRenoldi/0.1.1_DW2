d972742912a17017b6985b81ea836b769e9f7581
LeonardoRenoldi <futebol668@gmail.com>
2022-04-29 22:17:00 -0300

    1 file added

---
+++ 2.2/index.js
@@ -0,0 +1,55 @@
+const Sistema = [
+
+
+  Usuarios = {Nome: 'Carlos',
+              NickName: 'xaolinMatadorDePorco',
+              Email:'CarlosRibeiro@gmail.com',
+              Senha:'abc123@',
+              id:1
+
+              ,Nome: 'Noronha',
+              NickName: 'VidaLok4',
+              Email:'Noronha@gmail.com',
+              Senha:'abc123@!!',
+              id:2}
+
+,Artigos = {
+Artigo = {
+NomeArtigo : 'Elon Musk compra Twitter!!',
+id : 1,
+Autor : NickName
+}
+
+,Artigo = {
+  NomeArtigo : 'Orkut vai voltar!!',
+  id : 2,
+  Autor : NickName
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
