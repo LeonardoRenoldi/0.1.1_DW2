d0aff414397cc952e333a854416dc50a0e14ae05
LeonardoRenoldi <futebol668@gmail.com>
2022-07-08 21:24:07 -0300

    1 file modified, 1 added

---
+++ 1.8.3/Resposta.js
@@ -0,0 +1,9 @@
+//URL: https://www.auxiliadorapredial.com.br/alugar/residencial/rs+porto-alegre+cidade-baixa?quartos=2&sem-fiador-ou-garantias=true//
+
+const protocolo = 'https://';
+
+const dominio = 'www.auxiliadorapredial.com.br';
+
+const paths = ['/alugar/', 'residencial/', 'rs+porto-alegre+cidade-baixa'];
+
+const Querys = ['?quartos=2', 'sem-fiador-ou-garantias=true'];
