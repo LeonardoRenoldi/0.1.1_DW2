a34bf5f00ff7df3efcab8d28f8980a2ce8d75a03
LeonardoRenoldi <futebol668@gmail.com>
2022-07-28 08:20:03 -0300

    1 file added

---
+++ 0.3.1/index.js
@@ -0,0 +1,23 @@
+const filmes = [
+  {
+    nome: 'O Senhor do Aneis: A Sociedade do Anel',
+    lancamento: 2002,
+    diretores: ['Peter Jackson'],
+    generos: ['Fantasia', 'Aventura'],
+  },
+  {
+    nome: 'Harry Potter e a Pedra Filosofal',
+    lancamento: 2001,
+    diretores: ['Chris Columbus'],
+    generos: ['Fantasia'],
+  },
+  {
+    nome: 'Matrix',
+    lancamento: 1999,
+    diretores: ['Lana Wachowski', 'Lilly Wachowski'],
+    generos: ['Ação', 'Ficção Cientifica'],
+  },
+];
+
+const filtroNomes = filmes.filter((filmes) => filmes.nome || filmes.lancamento);
+console.log(filtroNomes);
