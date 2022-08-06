780f73de6103dd39d268564082485f378288e681
LeonardoRenoldi <futebol668@gmail.com>
2022-08-05 23:22:54 -0300

    2 files added

---
+++ 0.3.2/index.js
@@ -0,0 +1,19 @@
+const tarefas = [
+  { titulo: 'Passear com o cachorro', concluida: false, dias: 10 },
+  { titulo: 'Comprar leite', concluida: false, dias: 5 },
+  { titulo: 'Lavar louça', concluida: true, dias: 60 },
+];
+
+const tarefasConcluidas = tarefas.filter((tarefa) => tarefa.concluida);
+
+const numeroTarefasNaoConcluidas = tarefas.filter(
+  (tarefa) => !tarefa.concluida
+).length;
+
+const verificaTarefasAbaixo30Dias = tarefas.filter(
+  (tarefa) => tarefa.concluida && tarefa.dias <= 30
+);
+
+console.log(tarefasConcluidas);
+console.log(numeroTarefasNaoConcluidas);
+console.log(verificaTarefasAbaixo30Dias);
---
+++ 0.3.3/index.js
@@ -0,0 +1,32 @@
+const medicos = [
+  {
+    nome: 'Leticia',
+    sobrenome: 'Costa',
+    genero: 'feminino',
+  },
+  {
+    nome: 'Adimaldo',
+    sobrenome: 'Pinto',
+    genero: 'masculino',
+  },
+  {
+    nome: 'Noah',
+    sobrenome: 'Lutus',
+    genero: 'nao-binario',
+  },
+];
+
+const nomeGenero = medicos.map(apresentacaoPorGenero);
+
+function apresentacaoPorGenero(medicos) {
+  if (medicos.genero === 'masculino') {
+    return `Dr ${medicos.nome} ${medicos.sobrenome}`;
+  }
+  if (medicos.genero === 'feminino') {
+    return `Dr(a) ${medicos.nome} ${medicos.sobrenome}`;
+  }
+  if (medicos.genero === 'nao-binario') {
+    return `Dr(e) ${medicos.nome} ${medicos.sobrenome}`;
+  }
+}
+console.log(nomeGenero);
