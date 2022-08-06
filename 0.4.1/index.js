4ffca36e5a13992cdf4662f1119418636218d0e8
LeonardoRenoldi <futebol668@gmail.com>
2022-08-05 23:25:46 -0300

    2 files added

---
+++ 0.4.1/index.js
@@ -0,0 +1,28 @@
+const alunos = [
+  {
+    nome: 'Mathias',
+    notas: [
+      { cadeira: 'DW2', nota: 10 },
+      { cadeira: 'BD2', nota: 7 },
+    ],
+  },
+  {
+    nome: 'Luiza',
+    notas: [
+      { cadeira: 'DW2', nota: 8 },
+      { cadeira: 'BD2', nota: 8 },
+    ],
+  },
+  {
+    nome: 'Pedro',
+    notas: [
+      { cadeira: 'DW2', nota: 10 },
+      { cadeira: 'BD2', nota: 10 },
+    ],
+  },
+];
+const notasTotais = alunos
+  .map((item) => item.notas)
+  .map((itens) => itens.reduce((acc, { nota }) => acc + nota, 0));
+
+console.log(alunos[0]);
